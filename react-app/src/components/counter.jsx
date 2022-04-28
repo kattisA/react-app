d7a5e60fddbd5776eb786fa9efce9e0aa16771cd
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2022-04-28 09:07:33 +0200

    Finalized tutorial

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    state = {
-        count: 0
-    };
+   
 
     styles = {
         fontSize: 50,
         fontWeight: 'bold'
     };
 
+
+
     render() { 
+        
         return  (
         <div>
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+            {this.props.children}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button 
+            onClick={() => this.props.onIncrement(this.props.counter)} 
+            className="btn btn-secondary btn-sm"
+            >Increment
+            </button>
+            <button 
+            onClick= {() =>this.props.onDelete(this.props.counter.id)} 
+            className="btn btn-danger btn-sm m-2">Delete</button>
+    
         </div>
         )
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount(){
-        const  { count } = this.state;
+        const  { value: count } = this.props.counter;
         return count === 0 ? 'Zero' : count;
     }
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
